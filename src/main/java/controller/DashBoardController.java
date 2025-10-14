d5e4c7e3e4906e5676999f13739f06cc64ba2af4
SarasiPrarthana <prarthanasarasi32@gmail.com>
2025-10-14 12:11:23 +0530

    Add more Cotrollers

--- src/main/java/controller/DashBoardController.java
+++ src/main/java/controller/DashBoardController.java
@@ -1,4 +1,28 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class DashBoardController {
+
+    @FXML
+    void btnCustomerAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnEmployeeAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnItemAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSupplierAction(ActionEvent event) {
+
+    }
+
 }
