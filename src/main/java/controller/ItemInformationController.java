a538c53bafa73dda2e439160a6e84bd619ed0939
SarasiPrarthana <prarthanasarasi32@gmail.com>
2025-10-19 08:15:54 +0530

    Create an Observable Array List

--- src/main/java/controller/ItemInformationController.java
+++ src/main/java/controller/ItemInformationController.java
@@ -1,12 +1,29 @@
 package controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import model.dto.ItemInfoDTO;
 
-public class ItemInformationController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ItemInformationController implements Initializable {
+
+    ObservableList<ItemInfoDTO> itemInfoDTOS = FXCollections.observableArrayList(
+            new ItemInfoDTO("1001","Red Rice 5kg","Groceries",40,1200.00),
+            new ItemInfoDTO("1001","Red Rice 5kg","Groceries",40,1200.00),
+            new ItemInfoDTO("1001","Red Rice 5kg","Groceries",40,1200.00),
+            new ItemInfoDTO("1001","Red Rice 5kg","Groceries",40,1200.00),
+            new ItemInfoDTO("1001","Red Rice 5kg","Groceries",40,1200.00),
+            new ItemInfoDTO("1001","Red Rice 5kg","Groceries",40,1200.00),
+            new ItemInfoDTO("1001","Red Rice 5kg","Groceries",40,1200.00)
+    );
 
     @FXML
     private TableColumn<?, ?> colCategory;
@@ -61,4 +78,8 @@ public class ItemInformationController {
 
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
