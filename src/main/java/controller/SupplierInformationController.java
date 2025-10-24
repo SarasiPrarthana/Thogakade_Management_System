b373908215de1737003bc7514c0242bd789da8e8
SarasiPrarthana <prarthanasarasi32@gmail.com>
2025-10-24 23:00:00 +0530

    Add the Skelton

--- src/main/java/controller/SupplierInformationController.java
+++ src/main/java/controller/SupplierInformationController.java
@@ -1,4 +1,88 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class SupplierInformationController {
+
+    @FXML
+    private TableColumn<?, ?> colAddress;
+
+    @FXML
+    private TableColumn<?, ?> colCity;
+
+    @FXML
+    private TableColumn<?, ?> colCompanyName;
+
+    @FXML
+    private TableColumn<?, ?> colEmail;
+
+    @FXML
+    private TableColumn<?, ?> colName;
+
+    @FXML
+    private TableColumn<?, ?> colPhone;
+
+    @FXML
+    private TableColumn<?, ?> colPostalCode;
+
+    @FXML
+    private TableColumn<?, ?> colProvince;
+
+    @FXML
+    private TableColumn<?, ?> colSupplierID;
+
+    @FXML
+    private TextField txtAddress;
+
+    @FXML
+    private TextField txtCity;
+
+    @FXML
+    private TextField txtCompanyName;
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private TextField txtPhone;
+
+    @FXML
+    private TextField txtPostalCode;
+
+    @FXML
+    private TextField txtProvince;
+
+    @FXML
+    private TextField txtSupplierID;
+
+    @FXML
+    private TableView<?> txtTbl;
+
+    @FXML
+    void btnAddAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnClearAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateAction(ActionEvent event) {
+
+    }
+
 }
